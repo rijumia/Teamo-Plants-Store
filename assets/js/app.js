2c90a3416972b10b18ac5aa1efa467a63bfa8b23
rijumia <riju.mia2612@gmail.com>
2025-02-13 23:03:35 +0600

    Implement fixed header on scroll and update navbar styles for improved navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 setInterval(function () {
-    let currentDate = new Date();
-    let dateString = currentDate.toLocaleString();
-    document.getElementById("showTime").innerHTML = dateString;
-  }, 1000);
+  let currentDate = new Date();
+  let dateString = currentDate.toLocaleString();
+  document.getElementById("showTime").innerHTML = dateString;
+}, 1000);
 
+window.addEventListener("scroll", function () {
+  let header = document.querySelector(".header_section nav");
+  if (window.scrollY > 50) {
+    header.classList.add("fixed-header");
+  } else {
+    header.classList.remove("fixed-header");
+  }
+});
